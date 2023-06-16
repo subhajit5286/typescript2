0c99dd494ce682650c02b3184fae55781b5e9b3d
subhajit5286 <subhajit.bhattacharyya@gmail.com>
2023-06-16 23:52:22 +0530

    node2 assignment

---
+++ app.ts
@@ -0,0 +1,16 @@
+//const express = require ('express');
+import express from 'express';
+
+import bodyParser from 'body-parser';
+
+import todosRoutes from './routes/todos';
+
+const app = express();
+app.use(bodyParser.json())
+app.use(todosRoutes);
+
+
+
+app.listen({port:3000},()=>{
+	console.log('server is running')
+})
